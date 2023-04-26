9067a12efa0e7eac07203c290277c205875ad2fb
Tippedbike <113185655+Tippedbike@users.noreply.github.com>
2023-04-26 09:04:12 -0400

    done

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject pipePrefab;
+    public GameObject Pipes;
     float randomHeight = 0.5f; 
     // Start is called before the first frame update
     void Start()
     {
-       InvokeRepeating("SpawnPipes", 2.0f, 3.0f);
+       InvokeRepeating("PipeSpawn", 2.0f, 3.0f);
        //Generates new pipes at an interval///
     }
 
@@ -21,6 +21,6 @@ public class SpawnManager : MonoBehaviour
 
     void PipeSpawn()
     { 
-       Instantiate( pipePrefab, new Vector2(2, Random.Range( 10f, 10f)), Quaternion.identity);
+       Instantiate( Pipes, new Vector2(2, Random.Range( -randomHeight, randomHeight)), Quaternion.identity);
     }
 }
