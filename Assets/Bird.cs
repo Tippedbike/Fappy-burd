b9fc8cd939b62a0435e142cf98d5903ad56f74dc
Tippedbike <113185655+Tippedbike@users.noreply.github.com>
2023-04-27 09:11:06 -0400

    lesson 4 but i need to fix the pipe spawnings they look trash

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bird : MonoBehaviour
 {
     Rigidbody2D bird;
+    int score = 0;
+    public Text scoreUI;
 
     // Start is called before the first frame update
     void Start()
     {
         bird = GetComponent<Rigidbody2D>();
-    
+
     }
 
     // Update is called once per frame
@@ -19,5 +22,15 @@ public class Bird : MonoBehaviour
        if(Input.GetKeyDown(KeyCode.Space)){
         bird.AddForce(new Vector2(0,1) * 450);
        } 
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      if(other.gameObject.CompareTag("Points")) 
+      {
+        //Increases score by 1//
+        score = score +1;
+
+        scoreUI.text = score.ToString();
+      }
     }
 }
